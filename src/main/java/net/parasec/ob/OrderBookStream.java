9613ca30d999026a03534062da18a4aa0cababa4
phil8192 <phil@parasec.net>
2019-11-23 16:32:29 +0000

    use new bitstamp websocket api

--- src/main/java/net/parasec/ob/OrderBookStream.java
+++ src/main/java/net/parasec/ob/OrderBookStream.java
@@ -1,57 +1,83 @@
 package net.parasec.ob;
 
-import net.parasec.trading.ticker.core.dispatch.EventListener;
-import net.parasec.trading.ticker.core.dispatch.EventQueue;
-import net.parasec.trading.ticker.core.wire.OrderEvent;
-import net.parasec.trading.ticker.core.Ticker;
-import net.parasec.trading.ticker.bitstamp.BitstampTicker;
+import net.parasec.trading.bitstampws.BitstampClient;
+import net.parasec.trading.bitstampws.BitstampMessageHandler;
+import net.parasec.trading.bitstampws.Client;
+import net.parasec.trading.bitstampws.OrderEvent;
 
+import net.parasec.trading.ticker.core.wire.Direction;
+import net.parasec.trading.ticker.core.wire.OrderInfo;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.apache.log4j.Level;
 
 
-public final class OrderBookStream implements EventListener<OrderEvent> { 
+public final class OrderBookStream implements BitstampMessageHandler<OrderEvent> {
 
-  public interface Evt {
-    void onUpdate(OrderBook ob);
-  };
+	private final OrderBook ob = new LinkedOrderBook();
 
-  private final OrderBook ob = new LinkedOrderBook();
-  private final Evt evt;
+	private String venue;
+	private String symbol;
 
-    
-  public OrderBookStream(final Evt evt) {
-    this.evt = evt;
-  }
 
-  public void onEvent(final OrderEvent oe) {
-    switch(oe.getState()) {
-      case CREATED: 
-        ob.addOrder(oe); 
-        break;
-      case UPDATED:
-        ob.modOrder(oe);
-        break;
-      case DELETED:
-        ob.delOrder(oe);
-        break;
-    }
-    evt.onUpdate(ob);
-  }
+	public OrderBookStream(String venue, String symbol) {
+		this.venue = venue;
+		this.symbol = symbol;
+	}
 
-  public static void main(final String[] args) throws Exception {
-    BasicConfigurator.configure();
-    Logger.getRootLogger().setLevel(Level.ERROR);
+	private int getLimitPrice(double price, String symbol) {
+		return Util.asCents(price);
+	}
 
-    final Ticker t = new BitstampTicker();
-    t.watchOrders(new EventQueue(new OrderBookStream(new OrderBookStream.Evt() {
-      public void onUpdate(final OrderBook ob) {
-        System.out.print("\u001b[2J\u001b[H");
-        System.out.println(ob);
-        System.err.println(ob.getState().toCsv());
-      }
-    })));
-  }
+	private long getVolume(double volume, String symbol) {
+		return Util.asSatoshi(volume);
+	}
+
+	@Override
+	public void onMessage(OrderEvent orderEvent) {
+		OrderEvent.Order order = orderEvent.order;
+		Direction direction = order.type == 0 ? Direction.BUY : Direction.SELL;
+		long localTs = System.currentTimeMillis();
+		String exchangeOrderId = Long.toString(order.id);
+		int limitPrice = getLimitPrice(order.price, symbol);
+		long volume = getVolume(order.amount, symbol);
+		long exchangeTimestamp = order.microTimestamp / 1000;
+
+		OrderInfo orderInfo = new OrderInfo(exchangeOrderId, limitPrice, volume, exchangeTimestamp);
+		net.parasec.trading.ticker.core.wire.OrderEvent.State state;
+
+		switch (orderEvent.event) {
+			case ORDER_CREATED:
+				state = net.parasec.trading.ticker.core.wire.OrderEvent.State.CREATED;
+				ob.addOrder(new net.parasec.trading.ticker.core.wire.OrderEvent(state, direction, symbol, venue, localTs, orderInfo));
+				break;
+			case ORDER_UPDATED:
+				state = net.parasec.trading.ticker.core.wire.OrderEvent.State.UPDATED;
+				ob.modOrder(new net.parasec.trading.ticker.core.wire.OrderEvent(state, direction, symbol, venue, localTs, orderInfo));
+				break;
+			case ORDER_DELETED:
+				state = net.parasec.trading.ticker.core.wire.OrderEvent.State.DELETED;
+				ob.delOrder(new net.parasec.trading.ticker.core.wire.OrderEvent(state, direction, symbol, venue, localTs, orderInfo));
+		}
+
+		System.out.print("\u001b[2J\u001b[H");
+		System.out.println(ob);
+		System.err.println(ob.getState().toCsv());
+	}
+
+	public static void main(final String[] args) throws Exception {
+		BasicConfigurator.configure();
+		Logger.getRootLogger().setLevel(Level.ERROR);
+		String symbol = args[0];
+		OrderBookStream orderBookStream = new OrderBookStream("bitstamp", symbol);
+		Client client = new BitstampClient();
+		String subscriptionId = client.subscribeOrders(symbol, orderBookStream);
+		try {
+			while (true) {
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+		}
+	}
 }
 
